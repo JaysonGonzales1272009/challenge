a8f159a818efb99eb48d112e959d1f324c94c77f
JaysonGonzales1272009 <125311522+JaysonGonzales1272009@users.noreply.github.com>
2023-02-16 09:17:24 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+basic.forever(function () {
+    if (input.compassHeading() < 45) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        if (input.compassHeading() > 110) {
+            basic.showLeds(`
+                . . # . .
+                . . # # .
+                # # # # #
+                . . # # .
+                . . # . .
+                `)
+        }
+    }
+})
+basic.forever(function () {
+	
+})
